1524db391198486e520185f7a91ee64ef7f3ddc8
Dharmesh-gohil <dharmesh.vatsalgohil@gmail.com>
2023-06-14 10:55:21 +0530

    changed maade

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Booklist() {
+  return (
+    <section className='booklist'>
+      <Book
+        src={firstBook.src}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        {/* this is use of children props which is used in  */}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          doloremque perferendis quae ea molestiae veritatis.
+        </p>
+        <button type='submit'>submit</button>
+      </Book>
+      <Book
+        src={secondBook.src}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const Book = () => {
+//   return (
+//     <article className='book'>
+//       <img src='./images/image5.jpg' alt='The legend of zelda' />
+//       <h1>
+//         The Legend of Zelda™: Tears of the Kingdom – The Complete Official
+//         Guide: Collector's Edition Hardcove
+//       </h1>
+//       <h2>Dharmesh Gohil</h2>
+//     </article>
+//   )
+// }
+const firstBook = {
+  src: './images/image-0.jpg',
+  author: 'Vatsal Gohil',
+  title: 'dont know',
+}
+const secondBook = {
+  src: './images/image2.jpg',
+  author: 'sonal chauhan',
+  title: 'bio graphy',
+}
+
+// const src = './images/image-0.jpg'
+// const author = 'Vatsal  Gohil'
+// const title =
+//   "The Legend of Zelda™: Tears of the Kingdom – The Complete OfficialGuide: Collector's Edition Hardcove"
+
+const Book = ({ src: image, title, author, children }) => {
+  // console.log(props)
+  // return (
+  //   <article className='book'>
+  //     <img src={props.src} alt={props.title} />
+  //     <h1>{props.title}</h1>
+  //     <h4>{props.author}</h4>
+  //     {/* {console.log(props)} */}
+  //   </article>
+  // ) or
+  // const { author, src: image, title } = props
+  return (
+    <article className='book'>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p> {children}</p>
+    </article>
+  )
+  // console.log(props) hrere we cant pass the props value
+}
+export default Booklist
+
+// const root = ReactDom.createRoot(document.getElementById('root')) or
+const root = ReactDom.createRoot(document.querySelector('#root'))
+root.render(<Booklist />)
---
+++ src/testing.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+const testing = () => {
+  return <div>testing</div>
+}
+
+export default testing
